ac423e4cf303eae2a813710fac0e9e7a4f4ef512
yuhki50 <yuhki50@gmail.com>
2019-12-08 16:38:55 +0900

    Add generate script

---
+++ generate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# require Python 3.8.0 or newer
+
+from pathlib import Path
+
+try:
+    script_dir = Path(__file__).parent.resolve()
+
+    with (script_dir / "fp-lib-table").open("w") as f:
+        f.write("\n".join([
+            "(fp_lib_table",
+            "\n".join([
+                f"""  (lib (name {v.stem})(type KiCad)(uri ${{KIPRJMOD}}/modules/{v.name})(options "")(descr ""))"""
+                for v in sorted(script_dir.glob("*.pretty"), key=lambda v: v.stem)
+            ]),
+            ")",
+        ]))
+
+except KeyboardInterrupt:
+    pass
